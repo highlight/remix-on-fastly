c6438b33c4a1187ea75c57b3e23a4389dd674dd9
Chris Esplin <chris@chrisesplin.com>
2023-08-04 10:45:58 -0600

    Attempting to pull in fastly:env

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,22 @@
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
 import type { MetaFunction } from '@fastly/remix-server-runtime';
+import { env } from 'fastly:env';
+import { json } from '@fastly/remix-server-runtime';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1',
 });
+
+export async function loader() {
+  return json({
+    ENV: {
+      HIGHLIGHT_PROJECT_ID: env('HIGHLIGHT_PROJECT_ID'),
+    },
+  });
+}
 
 export default function App() {
   return (
--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,2 +1,4 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@fastly/remix-runtime" />
+/// <reference types="@fastly/js-compute" />
+
